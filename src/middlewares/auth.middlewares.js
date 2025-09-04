02d3379b7db7d658bbfa6ef26947d99d54c8617d
DiogoTeixeiraDEV <diogocominteixeira@gmail.com>
2025-09-04 15:21:29 -0300

    adicionei middlewares para checar se o id do token é valido no db

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,7 +1,17 @@
 export async function authenticate(request, reply) {
   try {
-    await request.jwtVerify();   // o plugin já injeta o método
+    const decoded = await request.jwtVerify();   // o plugin já injeta o método
+
+    const userId = decoded.id;
+
+    const user = await db.user.findUnique ({where : {id: userId }})
+    if (!user){
+      return reply.code(401).send({error : "User not found"})
+    }
+
+    request.user = user;
   } catch (err) {
     return reply.status(401).send({ error: "Unauthorized" });
   }
 }
+
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,5 +2,5 @@ import * as authController from "../controllers/auth.controller.js";
 
 export async function authRoutes(fastify) {
   fastify.post("/auth/register", authController.registerUser);
-  fastify.post("/auth/login", authController.loginUser);
+  fastify.post("/auth/login", authController.loginUser);  
 }
